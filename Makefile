d588dcaefae4e9f372b1e1448af37d24464a54a5
Ben Ferguson <ferguson.ben86@gmail.com>
2021-04-10 11:15:02 -0400

    mac

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 # Makefile for MD / Genesis ROM
 # @RetroDevDiscord
 #==============================
-CC=m68k-elf-gcc -nostdlib -std=gnu11 -fno-pie -no-pie -fno-use-linker-plugin
-OBJCOPY=m68k-elf-objcopy
-LD=m68k-elf-ld 
-OBJDUMP=m68k-elf-objdump
+#CC=m68k-elf-gcc 
+CC=m68k-apple-macos-gcc
+CFLAGS=-nostdlib -std=gnu11 -fno-pie -no-pie -fno-use-linker-plugin
+#AS=m68k-elf-as
+AS=m68k-apple-macos-as
+#OBJCOPY=m68k-elf-objcopy
+OBJCOPY=m68k-apple-macos-objcopy
+#LD=m68k-elf-ld 
+LD=m68k-apple-macos-ld
+#OBJDUMP=m68k-elf-objdump
+OBJDUMP=m68k-apple-macos-objdump
 MKDIR=mkdir 
 BUILDDIR=build
 SHOWELF=1
@@ -13,15 +20,15 @@ OUTDIR=out
 PYTHON=python3
 LINKSCR=rom.ld
 
-%.bin: %.elf
+%.bin: %.o
 	${LD} -s -T${LINKSCR} -o ${OUTDIR}/$@ ${BUILDDIR}/$< 
 
-%.elf: %.s
-	${CC} -c -o ${BUILDDIR}/$@ ${BUILDDIR}/$<
+%.o: %.s
+	${CC} ${CFLAGS} -c -o ${BUILDDIR}/$@ ${BUILDDIR}/$<
 	@if [ ${SHOWELF} -eq 1 ]; then ${OBJDUMP} -dS build/$@ > build/$@txt; fi
 
 %.s: ${SRCDIR}/%.c
-	${CC} -S -c -o ${BUILDDIR}/$@ $<
+	${CC} ${CFLAGS} -S -c -o ${BUILDDIR}/$@ $<
 
 # Create the main rom and pad it
 main: header main.bin 
@@ -30,9 +37,8 @@ main: header main.bin
 
 # Assemble the header from byte listing
 header: DIRs rom.ld 
-	${CC} -o ${BUILDDIR}/header.elf ${SRCDIR}/header.s
-	@echo --But it's the header, so it's ok!
-	${OBJCOPY} -O binary ${BUILDDIR}/header.elf ${OUTDIR}/header.bin
+	${AS} -o ${BUILDDIR}/header.o ${SRCDIR}/header.s
+	${OBJCOPY} -O binary -j .text ${BUILDDIR}/header.o ${OUTDIR}/header.bin
 
 # Create build directory	
 DIRs:
--- src/header.s
+++ src/header.s
@@ -1,169 +1,169 @@
-//Vector Table - start of ROM
+|Vector Table - start of ROM
     .file   "header.s"
     .text
     .align 2
-//    .globl _start 
+|    .globl _start 
 
     .dc.l 0xfffffe00     
-//stack pointer start location, usually $FFFFFE00
+|stack pointer start location, usually $FFFFFE00
     .dc.l 0x00000200              
-//program start location
+|program start location
     .dc.l 0               
-//bus error
+|bus error
     .dc.l 0               
-//address error
+|address error
     .dc.l 0               
-//illegal instruction
+|illegal instruction
     .dc.l 0               
-//divide by zero
+|divide by zero
     .dc.l 0               
-// CHK exception
+| CHK exception
     .dc.l 0               
-// TRAPV
+| TRAPV
     .dc.l 0               
-// privilege violation
+| privilege violation
     .dc.l 0               
-// TRACE exception
+| TRACE exception
     .dc.l 0               
-// Line-A emu
+| Line-A emu
     .dc.l 0               
-// Line-F emu
+| Line-F emu
     .dc.l 0               
-// unused 0
+| unused 0
     .dc.l 0               
-// unused 1
+| unused 1
     .dc.l 0               
-// unused 2
+| unused 2
     .dc.l 0               
-// unused 3
+| unused 3
     .dc.l 0               
-// unused 4
+| unused 4
     .dc.l 0               
-// unused 5
+| unused 5
     .dc.l 0               
-// unused 6
+| unused 6
     .dc.l 0               
-// unused 7
+| unused 7
     .dc.l 0               
-// unused 8
+| unused 8
     .dc.l 0               
-// unused 9
+| unused 9
     .dc.l 0               
-// unused 10
+| unused 10
     .dc.l 0               
-// unused 11
+| unused 11
     .dc.l 0               
-// spurious exception
+| spurious exception
     .dc.l 0               
-// IRQ level 1
+| IRQ level 1
     .dc.l 0               
-// IRQ level 2
+| IRQ level 2
     .dc.l 0               
-// IRQ level 3
+| IRQ level 3
     .dc.l 0               
-// IRQ level 4 (HBLank)
+| IRQ level 4 (HBLank)
     .dc.l 0               
-// IRQ level 5
+| IRQ level 5
     .dc.l 0               
-// IRQ level 6 (VBlank)
+| IRQ level 6 (VBlank)
     .dc.l 0               
-// IRQ level 7
+| IRQ level 7
     .dc.l 0               
-// TRAP 00 exception
+| TRAP 00 exception
     .dc.l 0               
-// TRAP 01 exception
+| TRAP 01 exception
     .dc.l 0               
-// TRAP 02 exception
+| TRAP 02 exception
     .dc.l 0               
-// TRAP 03 exception
+| TRAP 03 exception
     .dc.l 0               
-// TRAP 04 exception
+| TRAP 04 exception
     .dc.l 0               
-// TRAP 05 exception
+| TRAP 05 exception
     .dc.l 0               
-// TRAP 06 exception
+| TRAP 06 exception
     .dc.l 0               
-// TRAP 07 exception
+| TRAP 07 exception
     .dc.l 0               
-// TRAP 08 exception
+| TRAP 08 exception
     .dc.l 0               
-// TRAP 09 exception
+| TRAP 09 exception
     .dc.l 0               
-// TRAP 10 exception
+| TRAP 10 exception
     .dc.l 0               
-// TRAP 11 exception
+| TRAP 11 exception
     .dc.l 0               
-// TRAP 12 exception
+| TRAP 12 exception
     .dc.l 0               
-// TRAP 13 exception
+| TRAP 13 exception
     .dc.l 0               
-// TRAP 14 exception
+| TRAP 14 exception
     .dc.l 0               
-// TRAP 15 exception
+| TRAP 15 exception
     .dc.l 0               
-// unused 12
+| unused 12
     .dc.l 0               
-// unused 13
+| unused 13
     .dc.l 0               
-// unused 14
+| unused 14
     .dc.l 0               
-// unused 15
+| unused 15
     .dc.l 0               
-// unused 16
+| unused 16
     .dc.l 0               
-// unused 17
+| unused 17
     .dc.l 0               
-// unused 18
+| unused 18
     .dc.l 0               
-// unused 19
+| unused 19
     .dc.l 0               
-// unused 20
+| unused 20
     .dc.l 0               
-// unused 21
+| unused 21
     .dc.l 0               
-// unused 22
+| unused 22
     .dc.l 0               
-// unused 23
+| unused 23
     .dc.l 0               
-// unused 24
+| unused 24
     .dc.l 0               
-// unused 25
+| unused 25
     .dc.l 0               
-// unused 26
+| unused 26
     .dc.l 0               
-// unused 27
+| unused 27
     .ascii "SEGA MEGA DRIVE " 
-// console ID
+| console ID
     .ascii "@RETRODEVDISCORD" 
-// sonic
+| sonic
     .ascii "TEST ROM                                        " 
-// Domestic name
+| Domestic name
     .ascii "TEST ROM                                        " 
-// International name
+| International name
     .ascii "GM 00000000-00"   
-// Serial/version number (Rev 0)
+| Serial/version number (Rev 0)
     .dc.w 0x0
     .ascii "J               " 
-// I/O support
+| I/O support
     .dc.l 0x200		
-// Start address of ROM
+| Start address of ROM
     .dc.l 0x1ffff
-// End address of ROM
+| End address of ROM
     .dc.l 0xFF0000		
-// Start address of RAM
+| Start address of RAM
     .dc.l 0xFFFFFF		
-// End address of RAM
-//if EnableSRAM=1
-//		dc.b 0x52, 0x41, 0xA0+(BackupSRAM<<6)+(AddressSRAM<<3), 0x20
-//else
+| End address of RAM
+|if EnableSRAM=1
+|		dc.b 0x52, 0x41, 0xA0+(BackupSRAM<<6)+(AddressSRAM<<3), 0x20
+|else
 	.dc.l 0x20202020
-//endc
+|endc
     .dc.l 0x20202020		
-// SRAM start ($200001)
+| SRAM start ($200001)
     .dc.l 0x20202020		
-// SRAM end ($20xxxx)
+| SRAM end ($20xxxx)
     .ascii "                                                    " 
-// Notes (unused, anything can be put in this space, but it has to be 52 bytes.)
+| Notes (unused, anything can be put in this space, but it has to be 52 bytes.)
     .ascii "JUE             " 
-// Region (Country code)
-// (0x1ff)
+| Region (Country code)
+| (0x1ff)
