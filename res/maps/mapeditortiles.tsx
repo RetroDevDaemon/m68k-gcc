d8de90a35b865caf72e78c719149231463bb14a8
bferguson3 <ferguson.ben86@gmail.com>
2021-06-02 13:27:48 -0400

    refactor

--- res/maps/mapeditortiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="mapeditortiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
- <image source="mapeditortiles.png" width="128" height="128"/>
-</tileset>
